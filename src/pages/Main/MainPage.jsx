60603ddaf9cf4c3355a4fd0a2f3d6d6cd6468bca
RonohP <ronopurity05@gmail.com>
2022-03-07 05:34:14 +0300

    link changes

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -9,34 +9,27 @@ import Podcast from '../Podcast/Podcast';
 import Blog from '../Blog/Blog';
 import Communities from '../Communities/Communities';
 import FAQ from '../FAQ/faq';
-import Register from '../Register/Register';
-import Login from '../Login/Login';
-import AppState from '../../components/appstate';
+// import AppState from '../../components/appstate';
 
 function MainPage() {
   return (
-    <AppState>
     <div className='main'>
       <Sidebar />
       <div className='content'>
         <Searchbar />
 
         <Routes>
-        
-          <Route exact path={'/login'} element={<Login />} />
-				  <Route exact path={'/register'} element={<Register />} />
           <Route exact path={'/course'} element={<Course />} />
           <Route exact path={'/dashboard'} element={<Dashboard />} />
           <Route exact path={'/podcast'} element={<Podcast />} />
           <Route exact path={'/blog'} element={<Blog />} />
           <Route exact path={'/communities'} element={<Communities />} />
           <Route exact path={'/faq'} element={<FAQ />} />
-          <Route element={Dashboard} />
+          <Route element={<Dashboard />} />
         
         </Routes>
       </div>
     </div>
-    </AppState>
   );
 }
 
